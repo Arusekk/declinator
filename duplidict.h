0fe191430eb69e5763787c9646b4a44d8c4b43fe
Arusekk <arek_koz@o2.pl>
2018-05-10 13:14:11 +0200

    Fix duplidict.h

--- duplidict.h
+++ duplidict.h
@@ -4,29 +4,8 @@
 #include <json-c/json.h>
 #include <pcre.h>
 
-#define CF (PCRE_UCP|0)
-#define EF (PCRE_UCP&0)
-
-#if 0
-class DupliDict {
-	dict_t m_dict;
-
-public:
-	DupliDict();
-	DupliDict(const dict_t& x, DupliDict& par);
-
-	value_t& operator[](const key_t& key);
-	void _fixup(const dict_t& val);
-};
-
-class FSDict : public DupliDict {
-	std::string m_path;
-public:
-	FSDict(const std::string& path);
-
-	value_t& operator[](const key_t& key);
-};
-#endif
+#define CF (PCRE_UCP) /* default pcre_compile flags */
+#define EF (0) /* default pcre_exec flags */
 
 #define json_object_object_add_ex(o, k, v, x) json_object_object_add(o, k, v)
 
